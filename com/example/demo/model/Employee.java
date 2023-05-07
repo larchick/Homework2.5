89387e73cbc6faa3d970ccdac90b377deb9e30a9
larchick <liliyagreis@gmail.com>
2023-05-07 18:39:23 +0300

    сделано2

--- com/example/demo/model/Employee.java
+++ com/example/demo/model/Employee.java
@@ -19,6 +19,8 @@ public class Employee {
         return lastName;
     }
 
+    public String getFullName() { return firstName + " " + lastName; }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- com/example/demo/service/EmployeeServiceImpl.java
+++ com/example/demo/service/EmployeeServiceImpl.java
@@ -1,54 +1,54 @@
 package com.example.demo.service;
 
 import com.example.demo.exception.EmployeeAlreadyAddedException;
+import com.example.demo.exception.EmployeeNotFoundException;
 import com.example.demo.model.Employee;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
-    private final List<Employee> employeeList;
+    private final int EMPLOYEE_STORAGE_SITE = 5;
+    private Map<String, Employee> employees = new HashMap<>();
 
-    public EmployeeServiceImpl(List<Employee> employeeList) {
-        this.employeeList = employeeList;
+    public EmployeeServiceImpl() {
+        this.employees = new HashMap<>();
     }
+
     @Override
     public Employee add(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        if (employeeList.contains(employee)) {
+        if (employees.containsKey(employee.getFullName())) {
             throw new EmployeeAlreadyAddedException();
         }
-        employeeList.add(employee);
+
+        employees.put(employee.getFullName(), employee);
         return employee;
     }
 
     @Override
     public Employee remove(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        if (employeeList.contains(employee)) {
-            employeeList.remove(employee);
-            return employee;
+        if (employees.containsKey(employee.getFullName())) {
+            return employees.remove(employee.getFullName());
+
         }
-        throw new EmployeeAlreadyAddedException();
+        throw new EmployeeNotFoundException();
     }
 
     @Override
     public Employee find(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
 
-        if (employeeList.contains(employee)) {
-            return employee;
+        if (employees.containsKey(employee.getFullName())) {
+            return employees.get(employee.getFullName());
         }
-        throw new EmployeeAlreadyAddedException();
+        throw new EmployeeNotFoundException();
     }
 
     @Override
     public Collection<Employee> findAll() {
-        return Collections.unmodifiableList(employeeList);
+        return Collections.unmodifiableCollection(employees.values());
     }
-
-
 }
